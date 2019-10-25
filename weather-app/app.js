03bca771f2b1a67b1b56bac6729adfae7bb82db4
k0l3v <petarko@tryond.com>
2019-10-25 16:52:35 +0300

    40. Destructuring and Property Shorthand Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,18 +6,18 @@ const address = process.argv[2];
 if (!address) {
   console.log("Please provide an address");
 } else {
-  geocode(address, (err, data) => {
+  geocode(address, (err, { latitude, longitude, location }) => {
     if (err) {
       console.log(err);
       return;
     }
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return console.log(error);
       }
 
-      console.log(data.location);
+      console.log(location);
       console.log(forecastData);
     });
   });

4db4b5ad662bd841b4f45f21551954991d11ba33
k0l3v <petarko@tryond.com>
2019-10-28 14:01:30 +0200

    49. Advanced Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,26 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 const PORT = 3000;
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Weather App",
+    title: "Weather",
     name: "Petar Kolev"
   });
 });
@@ -19,14 +28,16 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Weather App",
+    name: "Petar Kolev",
     description: "NodeJS Weather App"
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "Help page",
-    message: "Halp meh!"
+    helpText: "Help page",
+    title: "Halp meh!",
+    name: "Pecimir"
   });
 });
 
